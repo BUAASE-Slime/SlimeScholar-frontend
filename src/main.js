de9674cde2b38f523d4e88f38b22a98c92bbfd0e
Zehuan Huang <zewanhuang@126.com>
2021-10-09 16:45:53 +0800

    feat: mount data stored in global.json globally

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import store from './store'
+
+import ElementUI from 'element-ui'
 
 Vue.config.productionTip = false
 
+// mount axios globally
+Vue.prototype.$axios = axios;
+// read data from public/global.json and mount it globally
+axios.get('./global.json').then((data) => {
+  Vue.prototype.GLOBAL = data.data;
+  axios.defaults.baseURL = data.data.backUrl;
+})
+// element UI
+Vue.use(ElementUI);
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
