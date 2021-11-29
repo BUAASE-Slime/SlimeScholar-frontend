3801daf070168f3225645c0b82122f36bd8b05e2
Zehuan Huang <zewanhuang@126.com>
2021-11-29 10:34:28 +0800

    fix(searchRes): duplicate value

--- src/views/retrieval/searchRes.vue
+++ src/views/retrieval/searchRes.vue
@@ -139,7 +139,6 @@
   export default {
     data() {
       return {
-        value1: false,
         resnum:45112,
         options: [{
           value: '1',
@@ -196,7 +195,6 @@
             title: "Knowledge-rich, computer-assisted composition of Chinese couplets",
             paperAbstract: "Recent research effort in poem composition has focused on the use of automatic language generation to produce a polished poem. A less explored question is how effectively a computer can serve as an interactive assistant to a poet. For this purpose, we built a web application that combines rich linguistic knowledge from classical Chinese philology with statistical natural language processing techniques. The application assists users in composing a ‘couplet’—a pair of lines in a traditional Chinese poem—by making suggestions for the next and corresponding characters. A couplet must meet a complicated set of requirements on phonology, syntax, and parallelism, which are challenging for an amateur poet to master. The application checks conformance to these requirements and makes suggestions for characters based on lexical, syntactic, and semantic properties. A distinguishing feature of the application is its extensive use of linguistic knowledge, enabling it to inform users of specific phonological principles in detail, and to explicitly model semantic parallelism, an essential characteristic of Chinese poetry. We evaluate the quality of poems composed solely with characters suggested by the application, and the coverage of its character suggestions.",
             citation_num: 3288,
-            reference_num: 14,
             comment_num: 5,
             year:2021,
             reference_num: 42,
@@ -222,7 +220,6 @@
             title: "Knowledge-rich, computer-assisted composition of Chinese couplets",
             paperAbstract: "Recent research effort in poem composition has focused on the use of automatic language generation to produce a polished poem. A less explored question is how effectively a computer can serve as an interactive assistant to a poet. For this purpose, we built a web application that combines rich linguistic knowledge from classical Chinese philology with statistical natural language processing techniques. The application assists users in composing a ‘couplet’—a pair of lines in a traditional Chinese poem—by making suggestions for the next and corresponding characters. A couplet must meet a complicated set of requirements on phonology, syntax, and parallelism, which are challenging for an amateur poet to master. The application checks conformance to these requirements and makes suggestions for characters based on lexical, syntactic, and semantic properties. A distinguishing feature of the application is its extensive use of linguistic knowledge, enabling it to inform users of specific phonological principles in detail, and to explicitly model semantic parallelism, an essential characteristic of Chinese poetry. We evaluate the quality of poems composed solely with characters suggested by the application, and the coverage of its character suggestions.",
             citation_num: 3288,
-            reference_num: 14,
             comment_num: 5,
             year:2021,
             reference_num: 42,
@@ -248,7 +245,6 @@
             title: "Knowledge-rich, computer-assisted composition of Chinese couplets",
             paperAbstract: "Recent research effort in poem composition has focused on the use of automatic language generation to produce a polished poem. A less explored question is how effectively a computer can serve as an interactive assistant to a poet. For this purpose, we built a web application that combines rich linguistic knowledge from classical Chinese philology with statistical natural language processing techniques. The application assists users in composing a ‘couplet’—a pair of lines in a traditional Chinese poem—by making suggestions for the next and corresponding characters. A couplet must meet a complicated set of requirements on phonology, syntax, and parallelism, which are challenging for an amateur poet to master. The application checks conformance to these requirements and makes suggestions for characters based on lexical, syntactic, and semantic properties. A distinguishing feature of the application is its extensive use of linguistic knowledge, enabling it to inform users of specific phonological principles in detail, and to explicitly model semantic parallelism, an essential characteristic of Chinese poetry. We evaluate the quality of poems composed solely with characters suggested by the application, and the coverage of its character suggestions.",
             citation_num: 3288,
-            reference_num: 14,
             comment_num: 5,
             year:2021,
             reference_num: 42,
@@ -289,51 +285,48 @@
       width:100%; 
       margin:0 auto;
   }
-</style>
-
-<style>
- #inputbox2 .el-scrollbar__wrap {
+  #inputbox2 >>> .el-scrollbar__wrap {
     overflow-x: hidden;
   }
-  #inputbox2 .el-input__inner{
+  #inputbox2 >>> .el-input__inner{
     height: 60px !important;
     border-right: none;
   }
-  #inputbox2 .el-input-group__append {
+  #inputbox2 >>> .el-input-group__append {
     /* border-radius: inherit; */
     background-color: white;
   }
-  #inputbox2 .el-input-group__prepend {
+  #inputbox2 >>> .el-input-group__prepend {
     /* border-radius: inherit; */
     background-color: white;
     /* color: azure; */
   }
-  #inputbox2 .el-row--flex {
+  #inputbox2 >>> .el-row--flex {
     display: flex;
     margin-top: 100px !important;
   }
-  #inputbox2 .pre .el-input__inner::placeholder {
-      color: black;
-      font-size: 18px;
-      text-align: center;
+  #inputbox2 .pre >>> .el-input__inner::placeholder {
+    color: black;
+    font-size: 18px;
+    text-align: center;
   }
-    /* 谷歌 */
-  #inputbox2 .pre .el-input__inner::-webkit-input-placeholder {
-      color: black;
-      font-size: 18px;
-      text-align: center;
+  /* 谷歌 */
+  #inputbox2 .pre >>> .el-input__inner::-webkit-input-placeholder {
+    color: black;
+    font-size: 18px;
+    text-align: center;
   }
-    /* 火狐 */
-  #inputbox2 .pre .el-input__inner:-moz-placeholder {
-      color: black;
-      font-size: 18px;
-      text-align: center;
+  /* 火狐 */
+  #inputbox2 .pre >>> .el-input__inner:-moz-placeholder {
+    color: black;
+    font-size: 18px;
+    text-align: center;
   }
-    /*ie*/
-  #inputbox2 .pre .el-input__inner:-ms-input-placeholder {
-      color: black;
-      font-size: 18px;
-      text-align: center;
+  /*ie*/
+  #inputbox2 .pre >>> .el-input__inner:-ms-input-placeholder {
+    color: black;
+    font-size: 18px;
+    text-align: center;
   }
   #the_item{
     height: 300px;
