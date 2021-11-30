9b71f9c7c1aaac10ef9c5b26667c5cfcdb227986
Zehuan Huang <zewanhuang@126.com>
2021-11-30 09:27:11 +0800

    feat(searchRes): connected

--- src/views/retrieval/searchRes.vue
+++ src/views/retrieval/searchRes.vue
@@ -2,16 +2,16 @@
   <div class="search-res">
     <div class="main-body">
       <el-row class="header">
-        <span style="float:left; font-size: 22px">
+        <span style="float:left; font-size: 22px; color: #A0A0A0">
         为您查询到
-        <span>&nbsp;{{ resNum }}&nbsp;</span>
+        <span style="color: #525455">&nbsp;{{ total_hits }}&nbsp;</span>
         条结果
         </span>
       </el-row>
       <div class="content-select-result">
         <el-row :gutter="0">
           <el-col :span="7"><div class="grid-content bg-purple" style="margin-right:50px">
-            <span style="display:flex; margin-bottom:30px; margin-top:5px; font-size:16px;">筛选</span>
+            <span style="display:flex; margin-bottom:24px; margin-top:10px; font-size:16px; color: #A0A0A0">筛选</span>
             <el-card class="box-card">
               <div>
                 <!-- <div style="float:left; width:100%; display:flex; margin:20px； float:left;"><span>
@@ -60,11 +60,11 @@
             <div>
               <el-row>
                 <el-col span="17">
-                  <span style="display:flex; font-size:16px; margin-top:5px">论文 ({{totalPaper}})</span>
+                  <span style="display:flex; font-size:16px; margin-top:10px;color: #A0A0A0">论文 ({{total_hits}})</span>
                 </el-col>
                 <el-col span="2">
-                  <div style="margin-top:5px">
-                    <span style="font-size:16px">排序</span>
+                  <div style="margin-top:10px">
+                    <span style="font-size:16px;color: #A0A0A0">排序</span>
                   </div>
                 </el-col>
                 <el-col span="5">
@@ -79,24 +79,27 @@
                 </el-col>
               </el-row>
             </div>
-            <el-card v-for="item in articles" :key="item" id="the_item" class="article-item">
+            <el-card v-for="item in articles" :key="item" class="article-item">
               <div style="text-align: left">
-                <h3>{{item.title}}</h3>
+                <div style="margin-bottom: 5px">
+                  <span style="font-size: 20px; font-weight: bold">{{item.paper_title}}</span>
+                </div>
                 <span style="color:grey;">{{item.year}} · </span>
-                <span v-for="j in item.authors" :key="j" style="margin-left:10px; color:grey;">{{j.name}}</span>
+                <span v-for="j in item.authors" :key="j" style="margin-left: 6px; color: #2d94d4; cursor: pointer">
+                  {{j.author_name}}
+                </span>
               </div>
 
               <div style="text-align:left;margin-top:10px;">
 <!--                <span style="display:block; font-weight:bold; font-size:15px; margin-bottom:10px">摘要：<br></span>-->
-                <span>{{item.paperAbstract|ellipsis}}</span>
+                <span style="color: #A0A0A0">{{item.abstract|ellipsis}}</span>
               </div>
               <el-divider></el-divider>
-              <div style="text-align:left;">
+              <div style="text-align:left; font-size: 15px">
                 <div v-if="item.is_collect" @click="collectChange(item)" style="float:left"><i class="el-icon-star-on" >收藏</i></div>
                 <div v-else @click="collectChange(item)" style="float:left"><i class="el-icon-star-off" >收藏</i></div>
                 <i class="el-icon-collection" style="margin-left:30px; float:left">引用</i>
-                <span style="float:right; text-align:right;color:#66b1ff;">{{item.reference_num}}</span>
-                <span style="float:right; text-align:right;">被引次数：</span>
+                <span style="float:right; text-align:right;">被引次数：{{item.reference_count}}</span>
               </div>
             </el-card>
           </div></el-col>
@@ -110,14 +113,13 @@
   export default {
     data() {
       return {
-        resNum:45112,
+        total_hits:45112,
         select: '1',
         year:["2008","2009","2021"],
         journals:["所有","会议","期刊","书籍"],
         fields:["计算机视觉","计算机图形学","人工智能"],
         queue:["发表时间","引用次数"],
         value2:"发表时间",
-        totalPaper:288,
         minYear:1928,
         maxYear:2021,
         value1:2021,
@@ -125,75 +127,75 @@
           {
             authors: [
               {
-                name: "John Lee",
+                author_name: "John Lee",
                 ids: [
                   "3362353",
                 ]
               },
               {
-                name: "Zuo_zuo",
+                author_name: "Zuo_zuo",
                 ids: [
                   "19373260",
                 ]
               },
             ],
-            id: "4cd223df721b722b1c40689caa52932a41fcc223",
-            title: "Knowledge-rich, computer-assisted composition of Chinese couplets Knowledge-rich, computer-assisted composition of Chinese couplets",
-            paperAbstract: "Recent research effort in poem composition has focused on the use of automatic language generation to produce a polished poem. A less explored question is how effectively a computer can serve as an interactive assistant to a poet. For this purpose, we built a web application that combines rich linguistic knowledge from classical Chinese philology with statistical natural language processing techniques. The application assists users in composing a ‘couplet’—a pair of lines in a traditional Chinese poem—by making suggestions for the next and corresponding characters. A couplet must meet a complicated set of requirements on phonology, syntax, and parallelism, which are challenging for an amateur poet to master. The application checks conformance to these requirements and makes suggestions for characters based on lexical, syntactic, and semantic properties. A distinguishing feature of the application is its extensive use of linguistic knowledge, enabling it to inform users of specific phonological principles in detail, and to explicitly model semantic parallelism, an essential characteristic of Chinese poetry. We evaluate the quality of poems composed solely with characters suggested by the application, and the coverage of its character suggestions.",
+            paper_id: "4cd223df721b722b1c40689caa52932a41fcc223",
+            paper_title: "Knowledge-rich, computer-assisted composition of Chinese couplets Knowledge-rich, computer-assisted composition of Chinese couplets",
+            abstract: "Recent research effort in poem composition has focused on the use of automatic language generation to produce a polished poem. A less explored question is how effectively a computer can serve as an interactive assistant to a poet. For this purpose, we built a web application that combines rich linguistic knowledge from classical Chinese philology with statistical natural language processing techniques. The application assists users in composing a ‘couplet’—a pair of lines in a traditional Chinese poem—by making suggestions for the next and corresponding characters. A couplet must meet a complicated set of requirements on phonology, syntax, and parallelism, which are challenging for an amateur poet to master. The application checks conformance to these requirements and makes suggestions for characters based on lexical, syntactic, and semantic properties. A distinguishing feature of the application is its extensive use of linguistic knowledge, enabling it to inform users of specific phonological principles in detail, and to explicitly model semantic parallelism, an essential characteristic of Chinese poetry. We evaluate the quality of poems composed solely with characters suggested by the application, and the coverage of its character suggestions.",
             citation_num: 3288,
             comment_num: 5,
             year:2021,
-            reference_num: 42,
+            reference_count: 42,
             is_collect:true,
             // 是否收藏
           },
           {
             authors: [
               {
-                name: "John Lee",
+                author_name: "John Lee",
                 ids: [
                   "3362353",
                 ]
               },
               {
-                name: "Zuo_zuo",
+                author_name: "Zuo_zuo",
                 ids: [
                   "19373260",
                 ]
               },
             ],
-            id: "4cd223df721b722b1c40689caa52932a41fcc223",
-            title: "Knowledge-rich, computer-assisted composition of Chinese couplets",
-            paperAbstract: "Recent research effort in poem composition has focused on the use of automatic language generation to produce a polished poem. A less explored question is how effectively a computer can serve as an interactive assistant to a poet. For this purpose, we built a web application that combines rich linguistic knowledge from classical Chinese philology with statistical natural language processing techniques. The application assists users in composing a ‘couplet’—a pair of lines in a traditional Chinese poem—by making suggestions for the next and corresponding characters. A couplet must meet a complicated set of requirements on phonology, syntax, and parallelism, which are challenging for an amateur poet to master. The application checks conformance to these requirements and makes suggestions for characters based on lexical, syntactic, and semantic properties. A distinguishing feature of the application is its extensive use of linguistic knowledge, enabling it to inform users of specific phonological principles in detail, and to explicitly model semantic parallelism, an essential characteristic of Chinese poetry. We evaluate the quality of poems composed solely with characters suggested by the application, and the coverage of its character suggestions.",
+            paper_id: "4cd223df721b722b1c40689caa52932a41fcc223",
+            paper_title: "Knowledge-rich, computer-assisted composition of Chinese couplets",
+            abstract: "Recent research effort in poem composition has focused on the use of automatic language generation to produce a polished poem. A less explored question is how effectively a computer can serve as an interactive assistant to a poet. For this purpose, we built a web application that combines rich linguistic knowledge from classical Chinese philology with statistical natural language processing techniques. The application assists users in composing a ‘couplet’—a pair of lines in a traditional Chinese poem—by making suggestions for the next and corresponding characters. A couplet must meet a complicated set of requirements on phonology, syntax, and parallelism, which are challenging for an amateur poet to master. The application checks conformance to these requirements and makes suggestions for characters based on lexical, syntactic, and semantic properties. A distinguishing feature of the application is its extensive use of linguistic knowledge, enabling it to inform users of specific phonological principles in detail, and to explicitly model semantic parallelism, an essential characteristic of Chinese poetry. We evaluate the quality of poems composed solely with characters suggested by the application, and the coverage of its character suggestions.",
             citation_num: 3288,
             comment_num: 5,
             year:2021,
-            reference_num: 42,
+            reference_count: 42,
             is_collect:true,
             // 是否收藏
           },
           {
             authors: [
               {
-                name: "John Lee",
+                author_name: "John Lee",
                 ids: [
                   "3362353",
                 ]
               },
               {
-                name: "Zuo_zuo",
+                author_name: "Zuo_zuo",
                 ids: [
                   "19373260",
                 ]
               },
             ],
-            id: "4cd223df721b722b1c40689caa52932a41fcc223",
-            title: "Knowledge-rich, computer-assisted composition of Chinese couplets",
-            paperAbstract: "Recent research effort in poem composition has focused on the use of automatic language generation to produce a polished poem. A less explored question is how effectively a computer can serve as an interactive assistant to a poet. For this purpose, we built a web application that combines rich linguistic knowledge from classical Chinese philology with statistical natural language processing techniques. The application assists users in composing a ‘couplet’—a pair of lines in a traditional Chinese poem—by making suggestions for the next and corresponding characters. A couplet must meet a complicated set of requirements on phonology, syntax, and parallelism, which are challenging for an amateur poet to master. The application checks conformance to these requirements and makes suggestions for characters based on lexical, syntactic, and semantic properties. A distinguishing feature of the application is its extensive use of linguistic knowledge, enabling it to inform users of specific phonological principles in detail, and to explicitly model semantic parallelism, an essential characteristic of Chinese poetry. We evaluate the quality of poems composed solely with characters suggested by the application, and the coverage of its character suggestions.",
+            paper_id: "4cd223df721b722b1c40689caa52932a41fcc223",
+            paper_title: "Knowledge-rich, computer-assisted composition of Chinese couplets",
+            abstract: "Recent research effort in poem composition has focused on the use of automatic language generation to produce a polished poem. A less explored question is how effectively a computer can serve as an interactive assistant to a poet. For this purpose, we built a web application that combines rich linguistic knowledge from classical Chinese philology with statistical natural language processing techniques. The application assists users in composing a ‘couplet’—a pair of lines in a traditional Chinese poem—by making suggestions for the next and corresponding characters. A couplet must meet a complicated set of requirements on phonology, syntax, and parallelism, which are challenging for an amateur poet to master. The application checks conformance to these requirements and makes suggestions for characters based on lexical, syntactic, and semantic properties. A distinguishing feature of the application is its extensive use of linguistic knowledge, enabling it to inform users of specific phonological principles in detail, and to explicitly model semantic parallelism, an essential characteristic of Chinese poetry. We evaluate the quality of poems composed solely with characters suggested by the application, and the coverage of its character suggestions.",
             citation_num: 3288,
             comment_num: 5,
             year:2021,
-            reference_num: 42,
+            reference_count: 42,
             is_collect:true,
             // 是否收藏
           }
@@ -201,7 +203,39 @@
       }
     },
     created() {
-    this.resNum = this.resNum.toLocaleString();
+      let _query = this.$route.query;
+      let _search_key = Object.keys(_query)[0];
+      let _search_value = _query[_search_key];
+
+      let _loadingIns = this.$loading({fullscreen: true, text: '拼命加载中'});
+      const _formData = new FormData();
+      _formData.append(_search_key, _search_value);
+      this.$axios({
+        method: 'post',
+        url: '/es/query/paper/' + _search_key,
+        data: _formData
+      })
+      .then(res => {
+        _loadingIns.close();
+        switch (res.data.status) {
+          case 200:
+            this.articles = res.data.details;
+            this.total_hits = res.data.total_hits.toLocaleString();
+            break;
+          case 404:
+            this.total_hits = 0;
+            break;
+          case 500:
+            this.$message.error("系统发生错误，请联系管理员！");
+            setTimeout(() => {
+              this.$router.push("/");
+            }, 1500);
+            break;
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      })
     },
     methods:{
       collectChange:function(item){
@@ -215,8 +249,8 @@
       //超过20位显示 ...
       ellipsis: function(value) {
         if (!value) return "";
-        if (value.length > 500) {
-          return value.slice(0, 500) + "...";
+        if (value.length > 400) {
+          return value.slice(0,400) + "...";
         }
         return value;
       }
@@ -237,8 +271,15 @@
 .search-res .article-item {
   margin-top:20px;
   display:flex;
-  padding-left: 20px;
-  padding-right: 20px;
+}
+
+.search-res .article-item >>> .el-card__body {
+  width: 100%;
+  padding: 25px 30px 15px;
+}
+
+.search-res .article-item >>> .el-divider--horizontal {
+  margin: 10px 0;
 }
 
 </style>
