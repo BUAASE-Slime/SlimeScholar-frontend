f5c00dc5e1d85a552c15a00b3a5dd322c3bd25a8
luxia <allxst@me.com>
2021-12-07 15:15:59 +0800

    change some style

--- src/views/scholar/ApplySettle.vue
+++ src/views/scholar/ApplySettle.vue
@@ -2,74 +2,76 @@
   <div class="schPortal">
     <el-row class="info-div" style="padding: 30px">
       <el-steps :active="step - 1" finish-status="success" style="padding:20px 50px 0 50px">
-        <el-step title="输入个人信息">
+        <el-step title="个人信息">
         </el-step>
-        <el-step title="选择您发表的学术成果"></el-step>
+        <el-step title="选择学术成果"></el-step>
         <el-step title="完成"></el-step>
       </el-steps>
 
       <el-divider/>
 
       <el-row align="middle" justify="center" type="flex">
-        <el-col style="padding: 0 30px 0 30px">
+        <el-col align="center" style="padding: 0 30px 0 30px;">
           <!--第一步-->
-          <el-row :hidden="step !== 1" style="font-size: large">
+          <el-row :hidden="step !== 1" style="font-size: large; width:80%;">
             <el-row class="fill-in-row">
-              <el-col :span="5">
+              <el-col :span="8">
                 姓名
               </el-col>
-              <el-col :span="8">
+              <el-col :span="16">
                 <el-input v-model="applyInfo.author_name" placeholder="请输入姓名"></el-input>
               </el-col>
             </el-row>
             <el-row class="fill-in-row">
-              <el-col :span="5">
+              <el-col :span="8">
                 工作单位
               </el-col>
-              <el-col :span="8">
+              <el-col :span="16">
                 <el-input v-model="applyInfo.affiliation_name" placeholder="请输入工作单位"></el-input>
               </el-col>
             </el-row>
 
             <el-row class="fill-in-row">
-              <el-col :span="5">
+              <el-col :span="8">
                 电子邮箱
               </el-col>
-              <el-col :span="8">
+              <el-col :span="16">
                 <el-input v-model="applyInfo.work_email" placeholder="请输入电子邮箱"></el-input>
               </el-col>
             </el-row>
 
             <el-row class="fill-in-row">
-              <el-col :span="5">
+              <el-col :span="8">
                 研究领域
               </el-col>
-              <el-col :span="8">
+              <el-col :span="16">
                 <el-input v-model="applyInfo.fields" placeholder="多个研究领域用英文半角逗号分隔"></el-input>
               </el-col>
             </el-row>
 
             <el-row class="fill-in-row">
-              <el-col :span="5">
+              <el-col :span="8">
                 个人首页（可选）
               </el-col>
-              <el-col :span="8">
+              <el-col :span="16">
                 <el-input v-model="applyInfo.home_page" placeholder="请输入您的个人首页"></el-input>
               </el-col>
             </el-row>
-            <el-button type="primary" @click="submit_info"> 下一步</el-button>
+            <el-button style="margin-top: 20px" type="primary" @click="submit_info"> 下一步</el-button>
           </el-row>
           <!--  第一步结束    -->
 
           <!--      第二步开始-->
           <el-row :hidden="step !== 2">
-            <el-row justify="space-between" style="padding: 10px" type="flex">
-              <el-col>已根据您的姓名，检索出 <span
-                  style="font-weight: bolder; color: #217ad9; font-size: larger;">{{ paper_group.length }}</span>
+            <el-row justify="space-around" style="padding-bottom: 10px; width: 100%; " type="flex">
+              <el-col style="text-align: left">已根据您的姓名，检索出 <span
+                  style="font-weight: bolder; color: #217ad9; font-size: x-large; ">{{ paper_group.length }}</span>
                 个文章组，请选择：
               </el-col>
               <el-col>
-                <el-button type="primary" @click="confirm_choice"> 确认</el-button>
+                <el-row justify="end" style="padding-right: 30px" type="flex">
+                  <el-button type="primary" @click="confirm_choice"> 确认</el-button>
+                </el-row>
               </el-col>
             </el-row>
             <el-row>
@@ -79,7 +81,7 @@
                   <template slot="title">
                     <el-checkbox v-model="papers_group_state[index].selected" class="choose-paper-checkbox"
                                  style="padding-left: 20px"><span
-                        style="font-size: x-large;margin: 30px;font-weight: bolder;"
+                        style="font-size: x-large;margin: 10px;font-weight: bold;color: #2f2e2e"
                         v-bind="papers_group_state[index].selected">{{
                         papers.author_name
                       }}</span>
@@ -88,7 +90,8 @@
                   <div v-for="item in papers.papers" :key="item" class="article-item" style="margin-left: 50px">
                     <div style="text-align: left">
                       <div style="margin-bottom: 10px">
-                        <span class="paper-title" style="font-size: large; font-weight: bold">{{
+                        <span class="paper-title"
+                              style="font-size: 20px; font-family:'Times Roman',sans-serif ;color: rgba(53,53,53,0.94) ">{{
                             item.paper_title
                           }}</span>
                       </div>
@@ -110,7 +113,13 @@
           <!--step 3 start -->
           <el-row :hidden="step !== 3">
             <el-col>
-              <el-result icon="success" subTitle="您的申请将在3天内由管理员审核，请耐心等待" title="欢迎入驻">
+              <el-result icon="success"  title="">
+                <template slot="title">
+                  <span style="font-size: x-large">欢迎入驻</span>
+                </template>
+                <template slot="subTitle">
+                  <span style="font-size: large">您的申请将在3天内由管理员审核，请耐心等待</span>
+                </template>
                 <template slot="extra">
                   <el-button type="primary">返回</el-button>
                 </template>
@@ -257,6 +266,7 @@ export default {
 
 .fill-in-row {
   padding: 10px;
+  margin-bottom: 5px;
 }
 
 
