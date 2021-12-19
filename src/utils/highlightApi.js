9ab0208488fbe6798d31e40b44d6f8ca102cbc73
Zehuan Huang <zewanhuang@126.com>
2021-12-19 16:23:08 +0800

    feat(advSearch): highlight

--- src/utils/highlightApi.js
+++ src/utils/highlightApi.js
@@ -1,3 +1,5 @@
+import el from "element-ui/src/locale/lang/el";
+
 export default {
     methods: {
         highlight(data) {
@@ -18,7 +20,7 @@ export default {
                         item.author_affiliation.forEach((item1, index, arr) => {
                             arr[index] = this.brightKeyword(arr[index])
                         })
-                    if (this.header_select === "main" || this.header_select === "author_name") {
+                    if (this.header_select === "author_name") {
                         item.authors.forEach((item2) => {
                             item2.author_name = this.brightKeyword(item2.author_name)
                         })
@@ -30,12 +32,35 @@ export default {
                     }
                 } else if (this.searchValue) {
                     // 高级检索
-
+                    this.searchValue.forEach((ins) => {
+                        if (ins.type !== 3) {
+                            if (ins.category === 'main' || ins.category === 'title')
+                                item.paper_title = this.brightKeyword(item.paper_title, ins.content)
+                            if (ins.category === "main" || ins.category === "abstract")
+                                item.abstract = this.brightKeyword(item.abstract, ins.content)
+                            if (ins.category === "affiliation_name")
+                                item.author_affiliation.forEach((item1, index, arr) => {
+                                    arr[index] = this.brightKeyword(arr[index], ins.content)
+                                })
+                            if (ins.category === "author") {
+                                item.authors.forEach((item2) => {
+                                    item2.author_name = this.brightKeyword(item2.author_name, ins.content)
+                                })
+                            }
+                            if (ins.category === "main" || ins.category === "field"){
+                                item.fields.forEach((item3) => {
+                                    item3.name=this.brightKeyword(item3.name, ins.content)
+                                })
+                            }
+                        }
+                    })
                 }
             })
         },
-        brightKeyword (val) {
-            let keyword = this.input
+        brightKeyword (val, inVal) {
+            let keyword;
+            if (inVal) keyword = inVal;
+            else keyword = this.input;
             let start=val.toLowerCase().indexOf(keyword.toLowerCase())
             if (start !== -1) {
                 return val.replace(val.slice(start, start+keyword.length), `<font color='#f00'>${val.slice(start, start+keyword.length)}</font>`)
